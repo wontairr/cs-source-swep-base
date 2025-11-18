1a8d0726b710009df8d665284be3700eebe0a3f6
Wontairr <nathanaelcampo@outlook.com>
2025-11-18 11:48:19 -0600

    Set default value for ScopeMoveSpeed

--- lua/weapons/css_weapon_scoped_base.lua
+++ lua/weapons/css_weapon_scoped_base.lua
@@ -8,7 +8,7 @@ SWEP.HideCrosshair = true
 
 SWEP.ScopeLevels 		= { 40, 10 } -- Fov numbers for the scoping level
 
-SWEP.ScopeMoveSpeed = 150
+SWEP.ScopeMoveSpeed = nil -- Set this to a number in your SWEP code.
 
 function SWEP:SecondaryAttack()
 	self:Scope()
